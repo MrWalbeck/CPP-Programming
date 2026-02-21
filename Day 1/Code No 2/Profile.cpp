1e92b9b3b5135bfedb476b0a8ba01001bbf4dbbe
Mr Walbeck <shahyanali067@gmail.com>
2026-02-21 19:17:04 +0500

    Day 1: Fundamentals & Data Handling Variables: Learned to declare and initialize various data types like int, float, char, and bool to store information. Operators: Explored arithmetic operators (+, -, *, /, %) and logical operators for data manipulation. Assignment: Mastered assigning values to variables and using compound assignment operators (e.g., +=, -=) for cleaner code. Day 2: Program Flow & Decision Making Sequence Statements: Understood the default sequential execution of C++ code, where statements are processed line-by-line. Selection Statements: Implemented decision-making logic using if, else if, and else blocks. Conditional Logic: Used relational operators (==, !=, <, >) within selection structures to execute specific code based on user input or conditions.

---
+++ Day 1/Code No 2/Profile.cpp
@@ -0,0 +1,9 @@
+	#include <iostream>
+	using namespace std;
+	int main()
+{
+	cout<<"\nName: "<<"Syed Rameez Haider Rizvi";
+	cout<<"\nClass: "<<"BSCS";
+	cout<<"\nDepartment: "<<"Computer Science";
+	cout<<"\nUniversity: "<<"University of Agriculture, Faisalabad";
+}
---
+++ Day 2/Task -1/Task -1.cpp
@@ -0,0 +1,16 @@
+	#include <iostream>
+	using namespace std;
+	int main()
+	{
+		int num;
+		cout <<"Enter your value:";
+		cin >> num;
+			if(num%2==0)
+				{
+					cout<<"Your answer is even";
+				}
+			if(num%2!=0)
+				{
+					cout<<"Your answer is old";
+				}
+	}
